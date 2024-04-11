0bb961249e722a0eb63a9009ff4bef028379632f
gandy818 <gandy818@gmail.com>
2024-04-11 13:50:59 +0900

    Feat: 게시글 페이지 퍼블리싱

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,19 +1,59 @@
 import styled from 'styled-components';
+import { Stack, styled as MUIStyled } from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 
 const Post = () => {
   return (
-    <div>
-      <Title>노래를 찾습니다 도와줘</Title>
-      <Writer>닉네임</Writer>
-      <Info>2일 뷰:21K 댓:164</Info>
-      <div>__</div>
-      <div>어디선가 들은 응원가인것 같은데 안타 안타 안타안타</div>
-      <div>추:13 댓:164</div>
-    </div>
+    <PostContainer>
+      <TitleContainer>
+        <Title>노래를 찾습니다 도와줘</Title>
+        <Writer>블라블라</Writer>
+        <Info direction="row" spacing={2}>
+          <div>
+            <AccessTimeIcon fontSize="small" style={{ verticalAlign: 'middle' }} />
+            <InfoText>2일</InfoText>
+          </div>
+          <div>
+            <RemoveRedEyeOutlinedIcon fontSize="small" style={{ verticalAlign: 'middle' }} />
+            <InfoText>21k</InfoText>
+          </div>
+          <div>
+            <ChatBubbleOutlineRoundedIcon fontSize="small" style={{ verticalAlign: 'middle' }} />
+            <InfoText>164</InfoText>
+          </div>
+        </Info>
+      </TitleContainer>
+      <ContentContainer>
+        <ContentArea>
+          어디선가 들은 응원가인것 같은데 안타 안타 안타안타 <br /> 이런 노래였어 ㅠㅠ 혹시 제목 아는 사람?
+        </ContentArea>
+        <Info direction="row" spacing={2}>
+          <div>
+            <ThumbUpOffAltIcon fontSize="small" style={{ verticalAlign: 'middle' }} />
+            <InfoText>13</InfoText>
+          </div>
+          <div>
+            <ChatBubbleOutlineRoundedIcon fontSize="small" style={{ verticalAlign: 'middle' }} />
+            <InfoText>164</InfoText>
+          </div>
+        </Info>
+      </ContentContainer>
+    </PostContainer>
   );
 };
 
 export default Post;
+
+const PostContainer = styled.div`
+  margin-inline: 20px;
+`;
+
+const TitleContainer = styled.div`
+  padding-bottom: 20px;
+`;
 
 const Title = styled.h2`
   font-size: 24px;
@@ -26,6 +66,23 @@ const Writer = styled.div`
   line-height: 16px;
 `;
 
-const Info = styled.div`
-  
-`
+const Info = MUIStyled(Stack)({
+  fontSize: '12px',
+  marginTop: '16px',
+  color: '#94969b',
+});
+
+const InfoText = styled.p`
+  display: inline;
+  margin-left: 2px;
+`;
+
+const ContentContainer = styled.div`
+  border-top: 1px solid #eee;
+`;
+
+const ContentArea = styled.div`
+  margin: 24px 0px 56px;
+  font-size: 16px;
+  line-height: 1.75em;
+`;
